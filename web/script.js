f31bb773dfbf88d1fb2e58ea87903072f3daee92
Saurabh Singh <saurabhx04@gmail.com>
2021-01-02 21:13:18 +0530

    Adding script file to the web folder

---
+++ web/script.js
@@ -0,0 +1,18 @@
+/*let js = "amazing";
+if (js == 'amazing') alert("JavaScript is FUN"); */
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log("Saurabh");
+console.log("4");
+console.log("40 + 8 + 23 - 10");
+
+let firstName = "Saurabh";
+console.log(firstName);
+
+console.log('Jonas');
+
+let PI = 3.1415;
+console.log(PI);
+
+console.log(typeof null);
